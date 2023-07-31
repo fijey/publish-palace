d3c2c237470d1bb9a548871d11bd30bc18365cb7
fijey <fajarmukti9@gmail.com>
2023-07-31 22:40:48 +0700

    perbaikan

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -29,7 +29,7 @@
 
     let html = `
     <div class="df-ui-btn df-ui-pagemode" title="Dark Mode" onclick="toggleDarkMode()">
-        <i class="fas fa-moon"></i> <!-- Ikon bulan untuk dark mode -->
+        <i class="fas fa-moon"></i>
         <i class="fas fa-sun" style="display:none"></i><span class="p-1">Toggle Dark Mode</span>
     </div>
     `;
@@ -37,6 +37,7 @@
     setTimeout(() => {
       $('.more-container').append(html);
     }, 3000);
+
     setTimeout(() => {
       toggleDarkMode();
     }, 4000);
